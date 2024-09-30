a35d4485c14c40b455068135add82618e7cfcdf0
Henri Kim Pinheiro Rodrigues <hkimpr40@gmail.com>
2024-09-30 03:54:46 -0300

    remake Farbelog

---
+++ FarbeLog.ts
@@ -0,0 +1,53 @@
+import fs from "fs";
+
+let config; //put your config logic here
+
+const logFilePath = config.logPath;
+const offset = config.UTCOffset;
+
+fs.appendFileSync(logFilePath, "\n\n------ log start ------\n");
+
+function getUTCDate(): string {
+    const now = new Date();
+
+    now.setUTCHours(now.getUTCHours() + offset);
+    
+    const Day = `${now.getUTCDate()}`.padStart(2, '0');
+    const Month = `${now.getUTCMonth() + 1}`.padStart(2, '0');
+    const Year = now.getUTCFullYear();
+    const Hour = `${now.getUTCHours()}`.padStart(2, '0');
+    const Minute = `${now.getUTCMinutes()}`.padStart(2, '0');
+    const Seconds = `${now.getUTCSeconds()}`.padStart(2, '0');
+    const ms = `${now.getUTCMilliseconds()}`.padStart(3, '0');
+    
+    const timezone = offset >= 0 ? `UTC+${offset}` : `UTC${offset}`;
+    
+    return `${timezone} ${Month}/${Day}/${Year} - ${Hour}:${Minute}:${Seconds},${ms}`;
+}
+
+
+function logMessage(label: string, content: string, color: string, type: string, consoleType: string, showHour: boolean) {
+    const Clabel = `\x1b[1;30m${label}\x1b[0m`;
+    const Ccontent = `\x1b[1;37m${content}\x1b[0m`;
+    const Cstate = `\x1b[0;37m[\x1b[0m \x1b[${color}m${type}\x1b[0m \x1b[0;37m]\x1b[0m`;
+    const Cdate = showHour ? `\x1b[0;37m[\x1b[0m \x1b[1;35m${getUTCDate()}\x1b[0m \x1b[0;37m]\x1b[0m ` : "";
+    const Ctext = `${Cdate}${Cstate} ${Clabel} ${Ccontent}`;
+
+    const state = `[ ${type} ]`;
+    const date = showHour ? `[ ${getUTCDate()} ] ` : "";
+    const text = `${date}${state} ${label} ${content}`;
+    
+    console[consoleType](Ctext);
+    fs.appendFileSync(logFilePath, text + '\n');
+}
+
+const obj = {
+    ok: (label: string, content: string, hour: boolean = true) => logMessage(label, content, "1;32", "OK", "log", hour),
+    error: (label: string, content: string, hour: boolean = true) => logMessage(label, content, "1;31", "ERROR", "error", hour),
+    warning: (label: string, content: string, hour: boolean = true) => logMessage(label, content, "1;33", "WARNING", "warn", hour),
+    info: (label: string, content: string, hour: boolean = true) => logMessage(label, content, "1;36", "INFO", "info", hour)
+};
+
+obj.info("loading", "main.ts");
+
+export default obj;
--- README.md
+++ README.md
@@ -2,48 +2,12 @@
 
 ## JS and TS support
 
-- ## clone repository
+- ## Usage
 
-    to start, we need to clone the repository, use the following command:
+    Use the following command to clone repository:
 
     ~~~git
     git clone https://github.com/B0463/FarbeLog.git
     ~~~
 
-- ## test
-
-    - ### JavaScript
-
-        open the `./JavaScript` folder and run the `./model.js` file.
-
-    - ### TypeScript
-
-        open the `./TypeScript/dist` folder and run the `./model.js` file.
-
-- ## for to use
-
-    - ### JavaScript
-
-        open the `./user` folder and run the file `./JavaScript.bat` on Windows, or `./JavaScript.sh` on linux.
-
-    - ### TypeScript
-
-        open the `./user` folder and run the file `./TypeScript.bat` on Windows, or `./TypeScript.sh` on linux.
-
-- ## for contributor
-
-    - ### git
-
-        create the commits with the commitzen pattern.
-
-    - ### JavaScript
-
-        there is no dependency for JavaScript.
-    
-    - ### TypeScript
-
-        to get started, install the dependencies with the following command:
-
-        ~~~bash
-        npm install --save-dev
-        ~~~
+    Then you can chose the file (JS or TS).
